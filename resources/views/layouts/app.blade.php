f8a067cb938dd026b1c88cc26c3418779e21e9a5
片桐海斗 <katakai224@icloud.com>
2022-09-05 18:25:00 +0900

    middleware, abort(404)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,12 +20,12 @@
         <div class="min-h-screen bg-gray-100">
             @if(auth('admin')->user())
                 @include('layouts.admin-navigation')
-            @elseif(auth('owner')->user())
+            @elseif(auth('owners')->user())
                 @include('layouts.owner-navigation')
             @elseif(auth('users')->user())
                 @include('layouts.user-navigation')
             @endif
-            
+
             <!-- Page Heading -->
             <header class="bg-white shadow">
                 <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
