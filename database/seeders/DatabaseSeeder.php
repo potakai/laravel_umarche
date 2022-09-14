3091db57e0aa99e2870a71b11558ffae83a88bb0
片桐海斗 <katakai224@icloud.com>
2022-09-14 18:26:23 +0900

    stock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,9 @@ class DatabaseSeeder extends Seeder
             ShopSeeder::class,
             ImageSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class
+            ProductSeeder::class,
+            StockSeeder::class
+
         ]);
     }
 }
