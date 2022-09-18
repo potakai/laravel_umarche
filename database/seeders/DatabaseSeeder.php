c1390402dd6d007cb0a21711cec26c32094ad892
片桐海斗 <katakai224@icloud.com>
2022-09-18 20:31:55 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Stock;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,5 +27,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class
 
         ]);
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
     }
 }
