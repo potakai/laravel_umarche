cac63debc2474b175c825aebfcbc59e9e8d4ab80
片桐海斗 <katakai224@icloud.com>
2022-09-27 15:09:50 +0900

    stripeキャンセル処理

--- resources/views/user/checkout.blade.php
+++ resources/views/user/checkout.blade.php
@@ -6,7 +6,7 @@ const stripe = Stripe(publicKey)
 
     window.onload = function() { stripe.redirectToCheckout({
     sessionId: '{{ $session->id }}' }).then(function (result) {
-    window.location.href = '{{ route('user.cart.index') }}';    
+    window.location.href = '{{ route('user.cart.cancel') }}';    
     }); 
 }
 </script>
