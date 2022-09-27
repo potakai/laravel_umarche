233d44ef42535a0690e494379927c4f4a6ce525e
片桐海斗 <katakai224@icloud.com>
2022-09-27 18:28:25 +0900

    検索フォーム　カテゴリー

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Stock;
+use App\Models\PrimaryCategory;
 use Illuminate\Support\Facades\DB;
 
 class ItemController extends Controller
@@ -29,16 +30,22 @@ class ItemController extends Controller
         });
             
     }
-    public function index(Request $request){
-        
+    public function index(Request $request)
+    {
+        // dd($request);
+        $categories = PrimaryCategory::with('secondary')
+        ->get();
+
         $products = Product::availableItems()
+        ->selectCategory($request->category ?? '0')
         ->sortOrder($request->sort)
         ->paginate($request->pagination ?? '20');
         
         
         //dd($stocks, $products);
         //$products = Product::all();
-        return view('user.index', compact('products'));
+        return view('user.index', 
+        compact('products', 'categories'));
     }
 
     public function show($id)
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -109,5 +109,15 @@ class Product extends Model
             }
         
     }
+
+    public function scopeSelectCategory($query, $categoryId)
+    {
+        if($categoryId !== '0')
+        {
+            return $query->where('secondary_category_id', $categoryId);
+        } else {
+            return ;
+        }
+    }
 }
 
