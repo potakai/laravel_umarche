13466b02f310798e2693a16fd89ca722c5aa7cf9
片桐海斗 <katakai224@icloud.com>
2022-09-28 20:33:46 +0900

    オーナー向けメール送信

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Jobs\SendOrderedMail;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
@@ -59,15 +60,7 @@ class CartController extends Controller
 
     public function checkout()
     {
-        /////
-        $items = Cart::where('user_id',Auth::id())->get();
-        $products = CartService::getItemsInCart($items);
-        $user = User::findOrFail(Auth::id());
-
-        SendThanksMail::dispatch($products, $user);
-        dd('ユーザーメール送信テスト');
         
-        /////
 
         $user = User::findOrFail(Auth::id());
         $products = $user->products;
@@ -139,6 +132,19 @@ class CartController extends Controller
 
     public function success()
     {
+        /////
+        $items = Cart::where('user_id',Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        $user = User::findOrFail(Auth::id());
+
+        SendThanksMail::dispatch($products, $user);
+        foreach($products as $product)
+        {
+            SendOrderedMail::dispatch($product, $user);
+        }
+        //dd('ユーザーメール送信テスト');
+        
+        /////
         Cart::where('user_id',Auth::id())->delete();
 
         return redirect()->route('user.items.index');
